2680d21021572d585e26a68766f754999503ff6b
wouter pigge <wouterpigge@gmail.com>
2017-11-06 11:50:58 +0100

    made the script.js file

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// hover over enlarge image
+
+
+
+        $(document).ready(function() {
+            $(".container1").show(1000);
+        })
+
+        $(".signupbtn").click(function() {
+            $(".container1").hide(1000)
+            $(".subscribedText").show(1000)
+
+        })
+
+        $(".cancelbtn").click(function() {
+            $(".container1").hide(1000)
+            $(".notSubscribedText").show(1000)
+        })
+
+        $(".closeNewsletter").click(function() {
+            $(".subscribedText").hide(1000)
+            $(".notSubscribedText").hide(1000)
+        })
